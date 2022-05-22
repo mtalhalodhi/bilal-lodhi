25dfa4923dc75ec1681545c70da990a54987dba0
Talha Lodhi <mtalhalodhi@gmail.com>
2022-05-22 12:18:26 +0500

    basic 3d

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,14 @@
 <script>
-	let name = 'world';
+	let title = "Bilal Lodhi";
 </script>
 
-<h1>Hello {name}!</h1>
+<iframe
+	title="3D Scene"
+	src="https://my.spline.design/booleanscopy-fedff24aacd3fd991cb78b6d2ee46ade/"
+	style="position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:-1;"
+	frameborder="0"
+	width="100%"
+	height="100%"
+/>
+
+<h1>{title}</h1>
